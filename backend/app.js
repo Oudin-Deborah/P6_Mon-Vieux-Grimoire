a35e4a1095812986a3001d0e85b13b1444573d09
Deborah Oudin <oudin.deborah.83@gmail.com>
2026-07-15 12:55:35 +0200

    API GET OK

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,27 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res)=>{
-    res.json({message:"Votre requete a bien été reçue"})
-})
+app.use(express.json)
+
+const books = require("../public/data/data.json");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+  );
+  next();
+});
+app.post("api/books", (req,res,next)=>{
+  console.log(req.body);
+  res.status(201).json({message:"Livre ajouté"});
+});
+app.get("/api/books", (req, res, next) => {
+  res.status(200).json(books);
+});
 module.exports = app;
